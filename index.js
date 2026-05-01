d6bd96a192e2b28e84d3b99e7b1105441eb77cb7
Hana Spoustová <bernreih@gmail.com>
2026-05-01 08:46:48 +0200

    add delete and done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const taskInput = document.getElementById('taskInput');
 const taskList = document.getElementById('taskList');
 const addButton = document.getElementById('button_add');
+const deleteButton = document.querySelector('.button_delete');
 
 const AddTask = () => {
   const text = taskInput.value.trim();
@@ -12,14 +13,25 @@ const AddTask = () => {
   const li = document.createElement('li');
 
   li.innerHTML = `<span> ${text} </span>
-  <button class="button_delete">🗑</button>`;
+  <button type="button" class="button_delete">🗑</button>`;
 
   taskList.appendChild(li);
   taskInput.value = '';
 };
 
+taskList.addEventListener('click', (event) => {
+  const clicked = event.target;
+
+  if (clicked.classList.contains('button_delete')) {
+    clicked.closest('li').remove();
+    return;
+  }
+
+  clicked.closest('li').classList.toggle('done');
+});
+
 addButton.addEventListener('click', AddTask);
-taskInput.addEventListener('keydown', (e) => {
+taskInput.addEventListener('keydown', (event) => {
   if (event.key === 'Enter') {
     AddTask();
   }
